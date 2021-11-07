4c4434fab7f45af44e2e7b968a593a8a80519d51
Mirko Boruc <mkbu@demant.com>
2021-11-07 15:15:48 +0100

    ready project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Alternative from "./Alternative";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Alternative />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
